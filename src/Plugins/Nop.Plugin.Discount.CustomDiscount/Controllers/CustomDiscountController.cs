57e38f94f1adb402a3277d1549fd13ab8d2b4f11
Bhautik Patel <Bhautik.Patel@tmotions.com>
2025-04-14 19:55:19 +0530

    => Custom discount plugin: - Applies a custom discount based on customer order history
    - Added plugin
    - On install - Added discount, requirnment and locals
    - On Uninstall - Removed discount, local
    - Discount requirnment: Search customer's paid order
    - Added default class
    - Added logo
    - Added plugin.json

---
+++ src/Plugins/Nop.Plugin.Discount.CustomDiscount/Controllers/CustomDiscountController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Services.Discounts;
+using Nop.Services.Security;
+using Nop.Web.Framework;
+using Nop.Web.Framework.Controllers;
+using Nop.Web.Framework.Mvc.Filters;
+
+namespace Nop.Plugin.DiscountRules.CustomerRoles.Controllers;
+
+[AuthorizeAdmin]
+[Area(AreaNames.ADMIN)]
+[AutoValidateAntiforgeryToken]
+public class CustomDiscountController : BasePluginController
+{
+    #region Fields
+
+    protected readonly IDiscountService _discountService;
+    protected readonly IPermissionService _permissionService;
+
+    #endregion
+
+    #region Ctor
+
+    public CustomDiscountController(IDiscountService discountService,
+        IPermissionService permissionService)
+    {
+        _discountService = discountService;
+        _permissionService = permissionService;
+    }
+
+    #endregion
+
+    #region Methods
+
+    [CheckPermission(StandardPermission.Promotions.DISCOUNTS_VIEW)]
+    public async Task<IActionResult> Configure(int discountId, int? discountRequirementId)
+    {
+        //load the discount
+        var discount = await _discountService.GetDiscountByIdAsync(discountId)
+                       ?? throw new ArgumentException("Discount could not be loaded");
+
+        //check whether the discount requirement exists
+        if (discountRequirementId.HasValue && await _discountService.GetDiscountRequirementByIdAsync(discountRequirementId.Value) is null)
+            return Content("Failed to load requirement.");
+
+        //nothing on config page.
+
+        return View("~/Plugins/Discount.CustomDiscount/Views/Configure.cshtml");
+    }
+
+    #endregion
+}
