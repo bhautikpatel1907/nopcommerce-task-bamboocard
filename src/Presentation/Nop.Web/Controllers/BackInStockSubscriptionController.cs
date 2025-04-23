37f8793a84187980f01e6b26b8a5e466900333f4
Bhautik Patel <104594350+bhautikpatel1907@users.noreply.github.com>
2025-04-24 01:01:32 +0530

    Update BackInStockSubscriptionController.cs
    temp change

--- src/Presentation/Nop.Web/Controllers/BackInStockSubscriptionController.cs
+++ src/Presentation/Nop.Web/Controllers/BackInStockSubscriptionController.cs
@@ -14,6 +14,7 @@ using Nop.Web.Models.Common;
 
 namespace Nop.Web.Controllers;
 
+//temp
 [AutoValidateAntiforgeryToken]
 public partial class BackInStockSubscriptionController : BasePublicController
 {
